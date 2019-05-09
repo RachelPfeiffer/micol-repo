1988b2d2d0f2fa5c447e931f90158daf8f5a6fdf
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2019-05-09 21:47:20 +0300

    The first commit

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index.php" method="post">
+      <b>Name</b> <input type="text" name="name" value=""><br>
+      <b>Password</b> <input type="text" name="password" value="">
+      <button type="submit" name="button">Submit</button>
+    </form>
+
+  </body>
+</html>
